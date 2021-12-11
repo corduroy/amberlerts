984d875252cc76c66b0768345472cd5186247ff2
Joshua McKinnon <josh@corduroy.biz>
2021-12-11 22:08:54 +1100

    Added a detail view (empty)

---
+++ Shared/PriceDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  PriceDetailView.swift
+//  Amberlerts (iOS)
+//
+//  Created by Joshua McKinnon on 11/12/21.
+//
+
+import SwiftUI
+
+struct PriceDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PriceDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PriceDetailView()
+    }
+}
