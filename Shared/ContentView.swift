401de193472d98a1d12597d6d60aa6648951ddb3
Joshua McKinnon <josh@corduroy.biz>
2021-12-21 20:49:05 +1100

    Simpler interface stack with a pull-to-refresh

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,23 +8,34 @@
 import SwiftUI
 struct ContentView: View {
 	@EnvironmentObject var network: Network
-	
+	@Environment(\.colorScheme) var currentMode
+
 	let amberColor = Color(#colorLiteral(red: 0, green: 227/255, blue: 160/255, alpha: 1))
 	var body: some View {
 		NavigationView {
-			ScrollView{
-				LazyVStack() {
+			List {
 					let generalPrices = network.prices.filter{$0.channelType == "general"}
 					ForEach(generalPrices) { price in
 						PriceListItem(price: price)
 					}
-				}
 			}
 			.navigationTitle("Current Prices")
-			.foregroundColor(Color.black)
-			.background(Color.white)
-			.padding()
+			.listStyle(.plain)
+			.listRowSeparator(.hidden)
 		}
+		.refreshable {
+			do {
+				network.sites = try await network.getSites()
+			} catch {
+				print("Error Getting Sites", error)
+			}
+			do {
+				network.prices = try await network.getPrices()
+			} catch {
+				print("Error Getting Prices", error)
+			}
+		}
+
 		.task {
 			do {
 				network.sites = try await network.getSites()
@@ -38,7 +49,9 @@ struct ContentView: View {
 			}
 		}
 	}
+
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
